6a1625ae7ea7205887e284dfd124dfb70fac9eb8
nafiz1001 <nafiz.islam1001@gmail.com>
2023-06-21 15:46:12 -0400

    wip

--- main.py
+++ main.py
@@ -53,3 +53,81 @@ def to_dict_with_lineno(nodes: list[T_AST]) -> dict[int, T_AST]:
 appends = to_dict_with_lineno(AppendSelfWarningsVisitor().visit(tree))
 assigns = to_dict_with_lineno(AssignSelfWarningsVisitor().visit(tree))
 extends = to_dict_with_lineno(ExtendSelfWarningsVisitor().visit(tree))
+
+new_source_lines = []
+
+lineno_iter = range(len(source_lines))
+
+for lineno in lineno_iter:
+    source_line = source_lines[lineno]
+    if lineno in appends:
+        append = appends[lineno]
+    if lineno in assigns:
+        #     Assign(
+        # targets=[
+        #     Subscript(
+        #         value=Attribute(
+        #             value=Name(id='self', ctx=Load()),
+        #             attr='warnings',
+        #             ctx=Load()),
+        #         slice=Constant(value='individual'),
+        #         ctx=Store())],
+        # value=List(elts=[], ctx=Load())),
+        #     Assign(
+        # targets=[
+        #     Subscript(
+        #         value=Attribute(
+        #             value=Name(id='self', ctx=Load()),
+        #             attr='warnings',
+        #             ctx=Load()),
+        #         slice=Constant(value='name'),
+        #         ctx=Store())],
+        # value=JoinedStr(
+        #     values=[
+        #         Constant(value='Sample with the same name ['),
+        #         FormattedValue(
+        #             value=Subscript(
+        #                 value=Name(id='sample', ctx=Load()),
+        #                 slice=Constant(value='name'),
+        #                 ctx=Load()),
+        #             conversion=-1),
+        #         Constant(value='] already exists. A new sample with the same name will be created.')]))],
+        # Assign(
+        #     targets=[
+        #         Tuple(
+        #             elts=[
+        #                 Name(id='reference_genome_obj', ctx=Store()),
+        #                 Subscript(
+        #                     value=Attribute(
+        #                         value=Name(id='self', ctx=Load()),
+        #                         attr='errors',
+        #                         ctx=Load()),
+        #                     slice=Constant(value='reference_genome'),
+        #                     ctx=Store()),
+        #                 Subscript(
+        #                     value=Attribute(
+        #                         value=Name(id='self', ctx=Load()),
+        #                         attr='warnings',
+        #                         ctx=Load()),
+        #                     slice=Constant(value='reference_genome'),
+        #                     ctx=Store())
+        assign = assigns[lineno]
+        target = assign.targets[0]
+        if isinstance(target, ast.Tuple):
+            pass
+        elif isinstance(target, ast.Subscript):
+            value = assign.value
+            if isinstance(value, ast.List):
+                if not value.elts:
+                    # handle empty list value
+                    print(source_line)
+                else:
+                    value_content = value.elts[0]
+                    if isinstance(value_content, ast.Constant):
+                        # handle string literal
+                        tuple_node = ast.Tuple(elts=[ast.Constant(value_content)], ctx=ast.Load())
+                        
+    if lineno in extends:
+        extend = extends[lineno]
+    else:
+        pass
